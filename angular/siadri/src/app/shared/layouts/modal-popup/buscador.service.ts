be9ddd8302ee6942df4e959da4eddb299c3bc6df
Francisco Hurtado <francisco.hurtado@geoprocess.com.co>
2017-12-06 15:38:05 -0500

    merge

--- angular/siadri/src/app/shared/layouts/modal-popup/buscador.service.ts
+++ angular/siadri/src/app/shared/layouts/modal-popup/buscador.service.ts
@@ -31,32 +31,33 @@ export class BuscadorService {
   crearSolicitud(solicitud:any,nombreSol:any){
     this.ad.app.database().ref('/solicitudes').push(solicitud).then(data=>{
       const url = `https://us-central1-siadriuv.cloudfunctions.net/enviarCorreo`;
+      let mailData = {
+        para: 'francisco.hurtado@geoprocess.com.co',
+        asunto: 'prueba SIADRI',
+        mensaje: 'Hola, <br>  El usuario "+datosPedido.displayname+" ha solicitado el servicio con el nombre de :  con el codigo de servicio : . <br> ' +
+                  'Estos son los datos de contacto : <br> '+
+                  'Email: +datosPedido.email_solicitante+ <br> '+ 
+                  'Empresa: +datosPedido.empresa+ <br> '+ 
+                  'Telefono: +datosPedido.telefono+ <br> '+ 
+                  'Direccion: +datosPedido.direccion+ <br> '+ 
+                  'Nombre del solicitante: +datosPedido.displayname+<br> '+ 
+                  'Estos son los datos de la empresa solicitante :<br> '+
+                  'NIT: +datosPedido.nit_empresa+<br> '+
+                  'Telefono: +datosPedido.telefono_fijo+<br> '+
+                  'Tipo de empresa: +datosPedido.tipo_empresa+<br> '+
+                  'Cantidad: +datosPedido.catidad_pruebas+<br> '+
+                  'Observaciones: +datosPedido.observaci<br> '+
+                  'Informacion de la prueba :<br> '+
+                  'Nombre de la prueba: +datosPedido.prueba_ensayo+<br> '+
+                  'Codigo de la prueba: +datosPedido.cod_ensayo+<br> '+
+                  'Costo unitario: +datosPedido.costo_prueba+<br> '+
+                  'Descripcion: +datosPedido.descripcion+<br> '+
+                  'Laboratorio que realiza la prueba: +datosPedido.nombre_laboratorio+<br> '+
+                  'Normas de la prueba: +datosPedido.normas_prueba+<br>'
+                  
+                }
       console.log('solicitud exitosa');
-          return this._http.post(url, {
-            para: 'francisco.hurtado@geoprocess.com.co',
-            asunto: 'prueba SIADRI',
-            mensaje: 'Hola, <br>  El usuario "+datosPedido.displayname+" ha solicitado el servicio con el nombre de :  con el codigo de servicio : . <br> ' +
-                      'Estos son los datos de contacto : <br> '+
-                      'Email: +datosPedido.email_solicitante+ <br> '+ 
-                      'Empresa: +datosPedido.empresa+ <br> '+ 
-                      'Telefono: +datosPedido.telefono+ <br> '+ 
-                      'Direccion: +datosPedido.direccion+ <br> '+ 
-                      'Nombre del solicitante: +datosPedido.displayname+<br> '+ 
-                      'Estos son los datos de la empresa solicitante :<br> '+
-                      'NIT: +datosPedido.nit_empresa+<br> '+
-                      'Telefono: +datosPedido.telefono_fijo+<br> '+
-                      'Tipo de empresa: +datosPedido.tipo_empresa+<br> '+
-                      'Cantidad: +datosPedido.catidad_pruebas+<br> '+
-                      'Observaciones: +datosPedido.observaci<br> '+
-                      'Informacion de la prueba :<br> '+
-                      'Nombre de la prueba: +datosPedido.prueba_ensayo+<br> '+
-                      'Codigo de la prueba: +datosPedido.cod_ensayo+<br> '+
-                      'Costo unitario: +datosPedido.costo_prueba+<br> '+
-                      'Descripcion: +datosPedido.descripcion+<br> '+
-                      'Laboratorio que realiza la prueba: +datosPedido.nombre_laboratorio+<br> '+
-                      'Normas de la prueba: +datosPedido.normas_prueba+<br>'
-                      
-                    });
+          return this._http.post(url, mailData);
       
     });
   }
