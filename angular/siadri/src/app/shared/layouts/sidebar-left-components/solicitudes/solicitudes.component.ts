b29d4ef4fd546c7ae61671bb6722ab22f6b68c59
Francisco Hurtado <francisco.hurtado@geoprocess.com.co>
2017-12-27 15:51:40 -0500

    merge

--- angular/siadri/src/app/shared/layouts/sidebar-left-components/solicitudes/solicitudes.component.ts
+++ angular/siadri/src/app/shared/layouts/sidebar-left-components/solicitudes/solicitudes.component.ts
@@ -28,7 +28,6 @@ export class SolicitudesComponent implements OnInit {
           }
         }).subscribe(data=>{
           this.solicitudes = data;
-          console.log(data);
       });
     
  }
